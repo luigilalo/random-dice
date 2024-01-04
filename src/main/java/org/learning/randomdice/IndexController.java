212a5b1e6d63b894848a00a840f093081a42f8f0
luigilalo <luigilaloe1@gmail.com>
2024-01-04 09:30:01 +0100

    Initial Commit

---
+++ src/main/java/org/learning/randomdice/IndexController.java
@@ -0,0 +1,4 @@
+package org.learning.randomdice;
+
+public class IndexController {
+}
---
+++ src/test/java/org/learning/randomdice/RandomDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.randomdice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomDiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
